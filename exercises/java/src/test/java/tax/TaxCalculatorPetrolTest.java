de2e6450972ac43c3f2ec85802fd315b2ce41dc6
jenni.foster <jenni.foster@autotrader.co.uk>
2019-01-21 11:25:24 +0000

    fixed broken tests

--- exercises/java/src/test/java/tax/TaxCalculatorPetrolTest.java
+++ exercises/java/src/test/java/tax/TaxCalculatorPetrolTest.java
@@ -17,7 +17,7 @@ public class TaxCalculatorPetrolTest {
 
     @Before
     public void setUp() {
-        taxCalculator = new DummyTaxCalculator();
+        taxCalculator = new PetrolTaxCalculator();
     }
 
     @Test
